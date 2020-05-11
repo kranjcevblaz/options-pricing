72ab6150016e73a530454eddfc6eacd998ea30fc
Blaz Kranjcev <6144896+kranjcevblaz@users.noreply.github.com>
2020-05-11 12:17:08 +0200

    added B-S option input

--- run_example.py
+++ run_example.py
@@ -1,6 +1,8 @@
 from option import Binomial
 from black_scholes import BlackScholes
 
+# choose inputs for Binomial or B-S methods
+
 bin_option = Binomial(100, 102, 0.01, 0.5, 200,
                     {'tk': 'AAPL', 'is_calc': True, 'is_call': True, 'use_garch': True, 'start_date': '2018-01-01',
                      'end_date': '2018-06-28', 'american': False})
